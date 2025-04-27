4fb8ef5757b2b7151c03e0993f63f6f5417702e6
Aheguy4ik <163100992+Aheguy4ik@users.noreply.github.com>
2025-04-27 15:13:10 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -1,9 +1,10 @@
-//Print functions declaration
+// Print functions declaration
 #include <fstream>
 #include <iostream>
 #include <string>
 
 void print(const std::string& text, std::ofstream& out);
-// Output text to stream (modified in master)
-void print(const std::string& text, std::ostream& out = std::cout);
+// Output text to file stream
 
+void print(const std::string& text, std::ostream& out = std::cout);
+// Output text to output stream (default: std::cout)
