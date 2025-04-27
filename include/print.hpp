ba78fb973c5cfa69bb7803f47d48d0c209d6c5b0
Aheguy4ik <matveiluzkov@inbox.ru>
2025-04-27 14:56:01 +0300

    Changed comments in master

--- include/print.hpp
+++ include/print.hpp
@@ -3,7 +3,7 @@
 #include <iostream>
 #include <string>
 
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out); // Modified in masterconst std::string& text, std::ofstream& out);
 // Output text to stream
-void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ostream& out); // Modified in masterconst std::string& text, std::ostream& out = std::cout);
 // Output text to stream
