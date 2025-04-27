f6e377d6ce2e0e0cc3608bee5812b14ec2901afb
Aheguy4ik <matveiluzkov@inbox.ru>
2025-04-27 14:25:23 +0300

    Added code comments

--- include/print.hpp
+++ include/print.hpp
@@ -1,6 +1,9 @@
+// Print functions declaration
 #include <fstream>
 #include <iostream>
 #include <string>
 
 void print(const std::string& text, std::ofstream& out);
+// Output text to stream
 void print(const std::string& text, std::ostream& out = std::cout);
+// Output text to stream
