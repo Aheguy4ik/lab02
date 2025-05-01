5c2963ff63367296d4dc08b30ef181afb44906e9
Aheguy4ik <matveiluzkov@inbox.ru>
2025-05-01 13:34:37 +0300

    Merge remote changes

--- include/print.hpp
+++ include/print.hpp
@@ -1,9 +1,9 @@
-// Print functions declaration
+//Print functions declaration
 #include <fstream>
 #include <iostream>
 #include <string>
 
-void print(const std::string& text, std::ostream& out); // Modified in masterconst std::string& text, std::ofstream& out);
-// Output text to stream
-void print(const std::string& text, std::ostream& out); // Modified in masterconst std::string& text, std::ostream& out = std::cout);
-// Output text to stream
+void print(const std::string& text, std::ofstream& out);
+// Output text to stream (modified in master)
+void print(const std::string& text, std::ostream& out = std::cout);
+
